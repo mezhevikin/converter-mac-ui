874d537ac95c10842f1646f5643c353a96a45461
mezhevikin <mezhevikin@gmail.com>
2022-10-06 17:42:39 +0400

    Add NavigatioBar

--- Converter/Controllers/ConverterController.swift
+++ Converter/Controllers/ConverterController.swift
@@ -7,10 +7,34 @@ class ConverterController: UIViewController {
         super.viewDidLoad()
         title = l("Currency converter")
         view.addBody(converteView, safe: true)
+        addNavigationBarButtons()
     }
     
     lazy var converteView = ConverterView {
         $0.backgroundColor = .red
+    }
+    
+    func addNavigationBarButtons() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "refresh"),
+            style: .plain,
+            target: self,
+            action: #selector(refresh)
+        )
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "settings"),
+            style: .plain,
+            target: self,
+            action: #selector(openSettings)
+        )
+    }
+    
+    @objc func refresh() {
+        
+    }
+    
+    @objc func openSettings() {
+        
     }
     
 }
